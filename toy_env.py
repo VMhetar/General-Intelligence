dd45ff0c5cb7d80b2ebece3c201f3d3129435cea
VMhetar <mhetarvarad@gmail.com>
2025-12-31 13:58:08 +0530

    edited in toy_env.py

--- toy_env.py
+++ toy_env.py
@@ -6,20 +6,20 @@ class ToyPhysicsEnv:
         self.reset()
 
     def reset(self):
-        self.state = torch.tensor([0.0, 0.0])  # [position, velocity]
+        # [position, velocity, gravity]
+        self.state = torch.tensor([0.0, 0.0, self.gravity])
         return self.state.clone()
 
     def step(self, action):
-        # action = force
-        pos, vel = self.state
+        pos, vel, g = self.state
         force = action.item()
 
-        vel = vel + force + self.gravity * 0.1
+        vel = vel + force + g * 0.1
         pos = pos + vel * 0.1
 
-        self.state = torch.tensor([pos, vel])
+        self.state = torch.tensor([pos, vel, g])
 
-        reward = -abs(pos)          # stay near 0
+        reward = -abs(pos)
         done = abs(pos) > 10.0
 
         return self.state.clone(), reward, done
