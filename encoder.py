abe53f0d6bae0ecec3c9c67dd1108879c2e656ce
Joseph Ayanda <ayandajoseph390@gmail.com>
2026-01-03 19:00:05 +0100

    Add type hints to forward method in IdentityEncoder

--- encoder.py
+++ encoder.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class IdentityEncoder(nn.Module):
-    def forward(self, obs):
+    def forward(self, obs: torch.Tensor) -> torch.Tensor:
         return obs
